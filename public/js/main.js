a9e4e001e459bef560cf1aa0a9cea4de93d83f59
Diogo Vieira <diogobeat@hotmail.com>
2018-05-12 11:09:41 +0100

    views inicial

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,14 @@ const numberInput = document.getElementById('number'),
       button = document.getElementById('button'),
       response = document.querySelector('.response');
 
-button.addEventListener('click', send, false);
+button.addEventListener('click', envioNotificacao, false);
 
 const socket = io();
 socket.on('smsStatus', function(data){
   response.innerHTML = '<h5>Text message sent to ' + data.number + '</h5>';
 })
 
-function send() {
+function envioNotificacao() {
   console.log('send');
   const number = numberInput.value.replace(/\D/g, '');
   const text = textInput.value;
