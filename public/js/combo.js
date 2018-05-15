f79eabb556b35a208d0b4860c538d36096d8aeb2
PoopFace98 <32722225+PoopFace98@users.noreply.github.com>
2018-05-15 16:44:49 +0100

    aparecimento da meteorologia- DONE
    -

--- public/js/combo.js
+++ public/js/combo.js
@@ -26,46 +26,56 @@ client.onMessageArrived = onMessageArrived;
 
 // called when the client connects
 function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
-    console.log("onConnect");
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
   client.subscribe(cheese);
-      console.log("sub"+cheese);
+  console.log("sub" + cheese);
 
 }
 
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-        console.log("onConnectionLost:" + responseObject.errorMessage);
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:" + responseObject.errorMessage);
 
-    }
+  }
 }
 var obj;
 // called when a message arrives
 function onMessageArrived(message) {
-    console.log("onMessageArrived:" + message.payloadString);
-    obj = JSON.parse(message.payloadString);
-    document.getElementById("DirecaoVento").innerHTML = obj.measurements[0].series.DirecaoVento[0];
-    document.getElementById("VelocidadeVento").innerHTML = obj.measurements[0].series.VelocidadeVento[0];
-    document.getElementById("Temperatura").innerHTML = obj.measurements[0].series.Temperatura[0];
-    document.getElementById("Pressao").innerHTML = obj.measurements[0].series.Pressao[0];
-    document.getElementById("Humidade").innerHTML = obj.measurements[0].series.Humidade[0];
-    document.getElementById("Visibilidade").innerHTML = obj.measurements[0].series.Visibilidade[0];
-    document.getElementById("NascerSol").innerHTML = obj.measurements[0].series.NascerSol[0];
-    document.getElementById("PorSol").innerHTML = obj.measurements[0].series.PorSol[0];
-    document.getElementById("ts").innerHTML = obj.measurements[0].ts;
+  console.log("onMessageArrived:" + message.payloadString);
+  obj = JSON.parse(message.payloadString);
+  document.getElementById("DirecaoVento").innerHTML = obj.measurements[0].series.DirecaoVento[0];
+  document.getElementById("VelocidadeVento").innerHTML = obj.measurements[0].series.VelocidadeVento[0];
+  document.getElementById("Temperatura").innerHTML = obj.measurements[0].series.Temperatura[0];
+  document.getElementById("Pressao").innerHTML = obj.measurements[0].series.Pressao[0];
+  document.getElementById("Humidade").innerHTML = obj.measurements[0].series.Humidade[0];
+  document.getElementById("Visibilidade").innerHTML = obj.measurements[0].series.Visibilidade[0];
+  document.getElementById("NascerSol").innerHTML = obj.measurements[0].series.NascerSol[0];
+  document.getElementById("PorSol").innerHTML = obj.measurements[0].series.PorSol[0];
+  document.getElementById("ts").innerHTML = obj.measurements[0].ts;
 
 }
 
 
 $('#combobox').on('changed.bs.select', function (e) {
   if (mutated == true) {
-      client.disconnect();
+    client.disconnect();
     console.log("disconnect");
-      mutated = false;
-           }
- mutated = true;
-           cheese = e.target.value;
-           client.connect({
-            onSuccess: onConnect});
-            mutated = true;})
+    mutated = false;
+  }
+  mutated = true;
+  cheese = e.target.value;
+  client.connect({
+    onSuccess: onConnect
+  });
+  mutated = true;
+})
+
+
+
+var elem = document.getElementById("combobox");
+elem.onchange = function () {
+  var hiddenDiv = document.getElementById("weathercard");
+  hiddenDiv.style.display = (this.value == "") ? "none" : "block";
+};
