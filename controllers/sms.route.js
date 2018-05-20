7d65d7f1277c756a0729d3744198664e2867eb7e
Diogo Vieira <diogobeat@hotmail.com>
2018-05-20 01:03:57 +0100

    fix sms

--- controllers/sms.route.js
+++ controllers/sms.route.js
@@ -8,34 +8,63 @@ const nexmo = new Nexmo({
 }, {
 	debug: true
 });
+const mysql = require('mysql');
+
 
 
 router.get('/', function(request, response){
-  model.list(function(dados){
     response.set('Content-Type', 'text/html');
     response.render('index', {
-      dados: dados
     })
-  })
 });
 
 
 
-const from = 'WFDAI';
-const to = '351912493365';
-const text = 'Dess';
-
 router.post('/sendSMS', function(request, response) {
-  nexmo.message.sendSms(from, to, text, (error, response) => {
-    if(error) {
-      throw error;
-    } else if(response.messages[0].status != '0') {
-      console.error(response);
-      throw 'Nexmo returned back a non-zero status';
-    } else {
-      console.log(response);
-    }
-  });
+  var sql = 'SELECT * from Regras_User WHERE UserID=2';
+		global.connection.query(sql, function(error, dados, fields){
+			if (error) throw error;
+      for (var i of dados){
+        const from = 'WFDAI';
+        const to = '351912493365';
+        const text = 'Temperatura superior a ' + i.Temperatura_user + 'graus em ' + i.localidade;
+        if(i.Temperatura_user > i.Temperatura){   
+        nexmo.message.sendSms(from, to, text, (error, response) => {
+          if(error) {
+            throw error;
+          } else if(response.messages[0].status != '0') {
+            console.error(response);
+            throw 'Nexmo returned back a non-zero status';
+          } else {
+            console.log(response);
+          }
+          response.re('/', {
+          })
+        });
+        
+      }else{
+        const from = 'WFDAI';
+        const to = '351912493365';
+        const text = 'Temperatura nao vai superar o valor dado pelo utilizador';
+        nexmo.message.sendSms(from, to, text, (error, response) => {
+          if(error) {
+            throw error;
+          } else if(response.messages[0].status != '0') {
+            console.error(response);
+            throw 'Nexmo returned back a non-zero status';
+          } else {
+            console.log(response);
+          }
+          
+          response.redirect('/', {
+          })
+        });
+        
+      }
+      }
+      
+    });
+   
 });
 
 
