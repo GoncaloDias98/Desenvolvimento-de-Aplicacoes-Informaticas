fbb6b9290c81b35d133caa8371514553a9553029
PoopFace98 <32722225+PoopFace98@users.noreply.github.com>
2018-06-12 23:49:57 +0100

    registo free

--- controllers/user.route.js
+++ controllers/user.route.js
@@ -12,49 +12,49 @@ const nexmo = new Nexmo({
 
 
 
-router.get('/', function(request, response) {
+router.get('/', function (request, response) {
 	var user = request.user;
-	if(request.isAuthenticated()){
-		model.list(function(users){
+	if (request.isAuthenticated()) {
+		model.list(function (users) {
 			response.set('Content-Type', 'text/html');
-			if(user.Pagamento === 'Pago'){
+			if (user.Pagamento === 'Pago') {
 				response.render('user_index', {
-					users : users
+					users: users
 				})
-			}else{
-				response.render('sub_index',{
-					users : users
+			} else {
+				response.render('sub_index', {
+					users: users
 				})
 			}
 		})
-	}else{
+	} else {
 		response.redirect('/');
 	}
-	})
+})
 
 
-router.get('/listar',  function(request, response) {
-	model.list(function(users) {
+router.get('/listar', function (request, response) {
+	model.list(function (users) {
 		response.set("Content-Type", "text/html");
 		response.render('users_list', {
 			users: users
 		});
-	});	
+	});
 });
 
-router.get('/empresa', function(request,response){
+router.get('/empresa', function (request, response) {
 	var user = request.user;
-	if(request.isAuthenticated()){
-		model.listempresa(user.Empresa, function(users){
+	if (request.isAuthenticated()) {
+		model.listempresa(user.Empresa, function (users) {
 			response.set("Content-Type", "text/html");
 			response.render('empresa_index', {
-	  		users : users
-  			})
+				users: users
+			})
 		})
 	}
 });
 
-router.get('/empresareg', function(request, response) {
+router.get('/empresareg', function (request, response) {
 	response.set("Content-Type", "text/html");
 	response.render('empresa_registar_users', {
 		isNew: true,
@@ -63,43 +63,61 @@ router.get('/empresareg', function(request, response) {
 	});
 });
 
-router.post('/empresareg', function(request, response) {
+router.post('/empresareg', function (request, response) {
 	var user = request.user;
-	model.read(user.Email, function(users){
-	request.checkBody('Nome', 'Nome should have between 5 and 10 chars').isLength({min: 5, max: 10});
-	request.checkBody('password', 'Password should have between 8 and 15 chars').isLength({min: 8, max: 15});
-	request.checkBody('Email', 'Email should have between 6 and 150 chars').isLength({min: 6, max: 150});
-	request.checkBody('NIF', 'NIF should have 9 chars').isLength({min: 9, max: 9});
-	request.checkBody('Contacto', 'Contacto should have between 0 and 150 chars').isLength({min: 0, max: 150});
-	request.checkBody('Morada', 'Morada should have between 0 and 20 chars').isLength({min: 0, max: 20});
-	var errors = request.validationErrors();	
-	if (errors) {
-		response.render('empresa_registar_users', {
-			isNew: true,
-			user: {},
-			errors: errors
+	model.read(user.Email, function (users) {
+		request.checkBody('Nome', 'Nome should have between 5 and 10 chars').isLength({
+			min: 5,
+			max: 10
 		});
-	}else{
-		var data = {
-			'Nome': request.body.Nome,
-			'Email': request.body.Email,
-			'NIF': request.body.NIF,
-			'Contacto': request.body.Contacto,
-			'Morada': request.body.Morada,
-			'tipo': "subscritor individual",
-			'password': request.body.password,
-			'empresa' : user.Empresa,
-			'UI': request.body.UI,
+		request.checkBody('password', 'Password should have between 8 and 15 chars').isLength({
+			min: 8,
+			max: 15
+		});
+		request.checkBody('Email', 'Email should have between 6 and 150 chars').isLength({
+			min: 6,
+			max: 150
+		});
+		request.checkBody('NIF', 'NIF should have 9 chars').isLength({
+			min: 9,
+			max: 9
+		});
+		request.checkBody('Contacto', 'Contacto should have between 0 and 150 chars').isLength({
+			min: 0,
+			max: 150
+		});
+		request.checkBody('Morada', 'Morada should have between 0 and 20 chars').isLength({
+			min: 0,
+			max: 20
+		});
+		var errors = request.validationErrors();
+		if (errors) {
+			response.render('empresa_registar_users', {
+				isNew: true,
+				user: {},
+				errors: errors
+			});
+		} else {
+			var data = {
+				'Nome': request.body.Nome,
+				'Email': request.body.Email,
+				'NIF': request.body.NIF,
+				'Contacto': request.body.Contacto,
+				'Morada': request.body.Morada,
+				'tipo': "subscritor individual",
+				'password': request.body.password,
+				'empresa': user.Empresa,
+				'UI': request.body.UI,
 			};
-		model.create(data, function(){
-			response.redirect('/users/empresa');
-		});
-	}	
-})
+			model.create(data, function () {
+				response.redirect('/users/empresa');
+			});
+		}
+	})
 });
 
 
-router.get('/registar', function(request, response) {
+router.get('/registar', function (request, response) {
 	response.set("Content-Type", "text/html");
 	response.render('user_registar', {
 		isNew: true,
@@ -107,22 +125,40 @@ router.get('/registar', function(request, response) {
 		errors: []
 	});
 });
-router.post('/registar', function(request, response) {
+router.post('/registar', function (request, response) {
 
-	request.checkBody('Nome', 'Nome should have between 5 and 10 chars').isLength({min: 5, max: 10});
-	request.checkBody('password', 'Password should have between 8 and 15 chars').isLength({min: 8, max: 15});
-	request.checkBody('Email', 'Email should have between 6 and 150 chars').isLength({min: 6, max: 150});
-	request.checkBody('NIF', 'NIF should have 9 chars').isLength({min: 9, max: 9});
-	request.checkBody('Contacto', 'Contacto should have between 0 and 150 chars').isLength({min: 0, max: 150});
-	request.checkBody('Morada', 'Morada should have between 0 and 20 chars').isLength({min: 0, max: 20});
-	var errors = request.validationErrors();	
+	request.checkBody('Nome', 'Nome should have between 5 and 10 chars').isLength({
+		min: 5,
+		max: 10
+	});
+	request.checkBody('password', 'Password should have between 8 and 15 chars').isLength({
+		min: 8,
+		max: 15
+	});
+	request.checkBody('Email', 'Email should have between 6 and 150 chars').isLength({
+		min: 6,
+		max: 150
+	});
+	request.checkBody('NIF', 'NIF should have 9 chars').isLength({
+		min: 9,
+		max: 9
+	});
+	request.checkBody('Contacto', 'Contacto should have between 0 and 150 chars').isLength({
+		min: 0,
+		max: 150
+	});
+	request.checkBody('Morada', 'Morada should have between 0 and 20 chars').isLength({
+		min: 0,
+		max: 20
+	});
+	var errors = request.validationErrors();
 	if (errors) {
 		response.render('user_registar', {
 			isNew: true,
 			user: {},
 			errors: errors
 		});
-	}else{
+	} else {
 		var data = {
 			'Nome': request.body.Nome,
 			'Email': request.body.Email,
@@ -133,29 +169,29 @@ router.post('/registar', function(request, response) {
 			'password': request.body.password,
 			'UI': request.body.UI,
 			'Pagamento': 'Pago',
-			};
-			const from = 'WFDAI ';
-			const to = '351' + request.body.Contacto;
-			const text = 'Efetue o pagamento no valor de 10€ para: PT50 XXXX XXXX XXXXXXXXXXX XX'
-			nexmo.message.sendSms(from, to, text, (error, response) =>{
-				if(error){
-				  throw(error);
-				}else if (response.messages[0].status != '0'){
-				  console.error(response);
-				  throw 'Nexmo returned back a non-zero status';
-				} else{
-				  console.log(response);
-				}   
-			}); 
-		model.create(data, function(){
+		};
+		const from = 'WFDAI ';
+		const to = '351' + request.body.Contacto;
+		const text = 'Efetue o pagamento no valor de 10€ para: PT50 XXXX XXXX XXXXXXXXXXX XX'
+		nexmo.message.sendSms(from, to, text, (error, response) => {
+			if (error) {
+				throw (error);
+			} else if (response.messages[0].status != '0') {
+				console.error(response);
+				throw 'Nexmo returned back a non-zero status';
+			} else {
+				console.log(response);
+			}
+		});
+		model.create(data, function () {
 			response.redirect('/');
 		});
-	
+
 
 	}
 });
 
-router.get('/registarfree', function(request, response) {
+router.get('/registarfree', function (request, response) {
 	response.set("Content-Type", "text/html");
 	response.render('free_registar', {
 		isNew: true,
@@ -163,22 +199,40 @@ router.get('/registarfree', function(request, response) {
 		errors: []
 	});
 });
-router.post('/registarfree', function(request, response) {
+router.post('/registarfree', function (request, response) {
 
-	request.checkBody('Nome', 'Nome should have between 5 and 10 chars').isLength({min: 5, max: 10});
-	request.checkBody('password', 'Password should have between 8 and 15 chars').isLength({min: 8, max: 15});
-	request.checkBody('Email', 'Email should have between 6 and 150 chars').isLength({min: 6, max: 150});
-	request.checkBody('NIF', 'NIF should have 9 chars').isLength({min: 9, max: 9});
-	request.checkBody('Contacto', 'Contacto should have between 0 and 150 chars').isLength({min: 0, max: 150});
-	request.checkBody('Morada', 'Morada should have between 0 and 20 chars').isLength({min: 0, max: 20});
-	var errors = request.validationErrors();	
+	request.checkBody('Nome', 'Nome should have between 5 and 10 chars').isLength({
+		min: 5,
+		max: 10
+	});
+	request.checkBody('password', 'Password should have between 8 and 15 chars').isLength({
+		min: 8,
+		max: 15
+	});
+	request.checkBody('Email', 'Email should have between 6 and 150 chars').isLength({
+		min: 6,
+		max: 150
+	});
+	request.checkBody('NIF', 'NIF should have 9 chars').isLength({
+		min: 9,
+		max: 9
+	});
+	request.checkBody('Contacto', 'Contacto should have between 0 and 150 chars').isLength({
+		min: 0,
+		max: 150
+	});
+	request.checkBody('Morada', 'Morada should have between 0 and 20 chars').isLength({
+		min: 0,
+		max: 20
+	});
+	var errors = request.validationErrors();
 	if (errors) {
 		response.render('free_registar', {
 			isNew: true,
 			user: {},
 			errors: errors
 		});
-	}else{
+	} else {
 		var data = {
 			'Nome': request.body.Nome,
 			'Email': request.body.Email,
@@ -188,26 +242,28 @@ router.post('/registarfree', function(request, response) {
 			'tipo': "free",
 			'password': request.body.password,
 			'UI': request.body.UI,
-			};
-			
-	
+		};
+		model.createfree(data, function () {
+			response.redirect('/');
+		});
+
 
 	}
 });
 
 
-router.get('/:UserID',  function(request, response) {
-	model.readEmpresas(request.params.UserID, function(user) {
-			response.set("Content-Type", "text/html");
-			response.render('empresa_edit_user', {
-				isNew: false,
-				user : user,
-				errors: []
-			})
+router.get('/:UserID', function (request, response) {
+	model.readEmpresas(request.params.UserID, function (user) {
+		response.set("Content-Type", "text/html");
+		response.render('empresa_edit_user', {
+			isNew: false,
+			user: user,
+			errors: []
 		})
-		});
+	})
+});
 
-router.post('/:UserID', function(request, response) {
+router.post('/:UserID', function (request, response) {
 	var data = {
 		'Nome': request.body.Nome,
 		'Email': request.body.Email,
@@ -218,71 +274,87 @@ router.post('/:UserID', function(request, response) {
 	if (errors) {
 		data.Nome = request.user.Nome;
 		response.render('empresa_edit_user', {
-			user: data, errors: errors
+			user: data,
+			errors: errors
 		})
-	}else{
-		model.updateFuncionario(request.params.UserID, data, function(){
+	} else {
+		model.updateFuncionario(request.params.UserID, data, function () {
 			response.redirect('/users/empresa');
 		});
 	}
 });
 
-router.get('/update_user', function(request, response) {
-	model.read(request.params.Email, function(user) {
+router.get('/update_user', function (request, response) {
+	model.read(request.params.Email, function (user) {
 		if (user != undefined) {
 			response.set("Content-Type", "text/html");
 			response.render('user_index', {
 				isNew: false,
 				user: user,
 				errors: []
-			})		
-		}else{
+			})
+		} else {
 			response.status(404).end();
 		}
-	})	
+	})
 });
 
-router.post('/update_user', function(request, response) {	
-	request.checkBody('Nome', 'Nome should have between 5 and 10 chars').isLength({min: 5, max: 10});
-	request.checkBody('password', 'Password should have between 8 and 15 chars').isLength({min: 8, max: 15});
-	request.checkBody('NIF', 'NIF should have 9 chars').isLength({min: 9, max: 9});
-	request.checkBody('Contacto', 'Contacto should have between 0 and 150 chars').isLength({min: 0, max: 150});
-	request.checkBody('Morada', 'Morada should have between 0 and 20 chars').isLength({min: 0, max: 20});
-	var errors = request.validationErrors();	
+router.post('/update_user', function (request, response) {
+	request.checkBody('Nome', 'Nome should have between 5 and 10 chars').isLength({
+		min: 5,
+		max: 10
+	});
+	request.checkBody('password', 'Password should have between 8 and 15 chars').isLength({
+		min: 8,
+		max: 15
+	});
+	request.checkBody('NIF', 'NIF should have 9 chars').isLength({
+		min: 9,
+		max: 9
+	});
+	request.checkBody('Contacto', 'Contacto should have between 0 and 150 chars').isLength({
+		min: 0,
+		max: 150
+	});
+	request.checkBody('Morada', 'Morada should have between 0 and 20 chars').isLength({
+		min: 0,
+		max: 20
+	});
+	var errors = request.validationErrors();
 	if (errors) {
 		response.render('user_index', {
 			isNew: true,
 			user: {},
 			errors: errors
 		});
-	}else{
+	} else {
 		var data = {
 			'Nome': request.body.Nome,
-			'password': request.body.password,				
+			'password': request.body.password,
 			'NIF': request.body.NIF,
 			'Contacto': request.body.Contacto,
 			'Morada': request.body.Morada,
 			'tipo': "",
 			'UI': request.body.UI,
-			};
-		model.update(request.body.Email, data, function(){
+		};
+		model.update(request.body.Email, data, function () {
 			response.redirect('/');
-		}); 
-	
+		});
+
 
 	}
 });
 
-router.get('/email/delete', function(request, response){
-	model.remove(request.params.email, function() {
+router.get('/email/delete', function (request, response) {
+	model.remove(request.params.email, function () {
 		response.redirect('/');
-	})	
+	})
 });
 
 /*Gravar Localizações Selecionadas */
 
 
-router.get('/create_preferencia', function(request, response) {
+router.get('/create_preferencia', function (request, response) {
 	response.set("Content-Type", "text/html");
 	response.render('user_index', {
 		isNew: true,
@@ -291,22 +363,21 @@ router.get('/create_preferencia', function(request, response) {
 	});
 });
 
-router.post('/', function(request, response) {
+router.post('/', function (request, response) {
 
-	var errors = request.validationErrors();	
+	var errors = request.validationErrors();
 	if (errors) {
 		response.render('user_index', {
 			isNew: true,
 			user: {},
 			errors: errors
 		});
-	}else{
-		var data = {
-			};
-		model.create(data, function(){
+	} else {
+		var data = {};
+		model.create(data, function () {
 			response.redirect('/');
 		});
-	
+
 
 	}
 });
@@ -316,37 +387,37 @@ router.post('/', function(request, response) {
 
 
 
-/* Ler Localizações Selecionadas */  
+/* Ler Localizações Selecionadas */
 
-router.get('/preferencias', function(request, response) {
-	model.readpreferencias(request.params.Email, function(user) {
+router.get('/preferencias', function (request, response) {
+	model.readpreferencias(request.params.Email, function (user) {
 		if (user != undefined) {
 			response.set("Content-Type", "text/html");
 			response.render('user_index', {
 				isNew: false,
 				user: user,
 				errors: []
-			})		
-		}else{
+			})
+		} else {
 			response.status(404).end();
 		}
-	})	
+	})
 });
 
-router.post('/preferencias', function(request, response) {	
-	var errors = request.validationErrors();	
+router.post('/preferencias', function (request, response) {
+	var errors = request.validationErrors();
 	if (errors) {
 		response.render('user_index', {
 			isNew: true,
 			user: {},
 			errors: errors
 		});
-	}else{
+	} else {
 
-		model.readpreferencias(request.body.Email, function(){
+		model.readpreferencias(request.body.Email, function () {
 			response.redirect('/');
-		}); 
-	
+		});
+
 
 	}
 });
--- models/user.model.js
+++ models/user.model.js
@@ -1,26 +1,26 @@
 const bcrypt = require('bcrypt');
 module.exports = {
 
-list(callback) {
+	list(callback) {
 		var sql = 'SELECT * from User';
-		global.connection.query(sql, function(error, rows, fields){
+		global.connection.query(sql, function (error, rows, fields) {
 			if (error) throw error;
 			callback(rows);
 		});
 	},
-	
-listempresa(empresa,callback) {
+
+	listempresa(empresa, callback) {
 		var sql = 'SELECT * from User where Empresa=? ';
-		global.connection.query(sql, [empresa], function(error, rows, fields){
+		global.connection.query(sql, [empresa], function (error, rows, fields) {
 			if (error) throw error;
 			callback(rows);
 		});
 	},
 
 
-read(email, callback) {
+	read(email, callback) {
 		var sql = "SELECT * from User where Email=?";
-		global.connection.query(sql, [email], function(error, rows, fields) {
+		global.connection.query(sql, [email], function (error, rows, fields) {
 			if (error) throw error;
 			callback(rows[0]);
 		});
@@ -28,89 +28,105 @@ read(email, callback) {
 
 	readEmpresas(UserID, callback) {
 		var sql = "SELECT * from User where UserID=?";
-		global.connection.query(sql, [UserID], function(error, rows, fields) {
+		global.connection.query(sql, [UserID], function (error, rows, fields) {
 			if (error) throw error;
 			callback(rows[0]);
 		});
 	},
 
-create(data, callback) {
-	var sql = "INSERT INTO User (Nome, Password, Email, NIF, Contacto, Morada, TipoUser, Empresa, UI, Pagamento) VALUES (?,?,?,?,?,?,?,?,?,?)";
-	var salt = bcrypt.genSaltSync(10);
-	var hash = bcrypt.hashSync(data.password, salt);	
-	global.connection.query(
-		sql, [data.Nome, hash, data.Email, data.NIF, data.Contacto, data.Morada, data.tipo, data.empresa, data.ui, data.Pagamento], function(error, rows, fields) {
-		if (error) throw error;
-		callback(rows[0]);
-		console.log(sql);
-	});
-},
-
-
-
-update(email, data, callback) {
-		var sql = "UPDATE `mydb`.`User` SET `Nome`=?, `Password`=?, `NIF`=?, `Contacto`=?, `Morada`=?, `TipoUser`=?, `Empresa`=?,`UI`=?  WHERE `Email`=?";
-		var salt = bcrypt.genSaltSync(10);		
+	create(data, callback) {
+		var sql = "INSERT INTO User (Nome, Password, Email, NIF, Contacto, Morada, TipoUser, Empresa, UI, Pagamento) VALUES (?,?,?,?,?,?,?,?,?,?)";
+		var salt = bcrypt.genSaltSync(10);
 		var hash = bcrypt.hashSync(data.password, salt);
 		global.connection.query(
-			sql, [data.Nome, hash,  data.NIF, data.Contacto, data.Morada, data.tipo, data.empresa, data.UI, email], function(error, rows, fields) {
+			sql, [data.Nome, hash, data.Email, data.NIF, data.Contacto, data.Morada, data.tipo, data.empresa, data.ui, data.Pagamento],
+			function (error, rows, fields) {
+				if (error) throw error;
+				callback(rows[0]);
+				console.log(sql);
+			});
+	},
+	
+	createfree(data, callback) {
+		var sql = "INSERT INTO User (Nome, Password, Email, NIF, Contacto, Morada, TipoUser, Empresa, UI) VALUES (?,?,?,?,?,?,?,?,?)";
+		var salt = bcrypt.genSaltSync(10);
+		var hash = bcrypt.hashSync(data.password, salt);
+		global.connection.query(
+			sql, [data.Nome, hash, data.Email, data.NIF, data.Contacto, data.Morada, data.tipo, data.empresa, data.ui],
+			function (error, rows, fields) {
+				if (error) throw error;
+				callback(rows[0]);
+				console.log(sql);
+			});
+	},
+
+
+
+	update(email, data, callback) {
+		var sql = "UPDATE `mydb`.`User` SET `Nome`=?, `Password`=?, `NIF`=?, `Contacto`=?, `Morada`=?, `TipoUser`=?, `Empresa`=?,`UI`=?  WHERE `Email`=?";
+		var salt = bcrypt.genSaltSync(10);
+		var hash = bcrypt.hashSync(data.password, salt);
+		global.connection.query(
+			sql, [data.Nome, hash, data.NIF, data.Contacto, data.Morada, data.tipo, data.empresa, data.UI, email],
+			function (error, rows, fields) {
+				if (error) throw error;
+				callback(rows[0]);
+			});
+	},
+
+	updateFuncionario(UserID, data, callback) {
+		var sql = 'UPDATE User SET Nome=?, Contacto=?, UI=? WHERE UserID=?';
+		global.connection.query(sql, [data.Nome, data.Contacto, data.UI, UserID], function (error, rows) {
 			if (error) throw error;
 			callback(rows[0]);
 		});
 	},
 
-updateFuncionario(UserID, data, callback){
-	var sql = 'UPDATE User SET Nome=?, Contacto=?, UI=? WHERE UserID=?';
-	global.connection.query(sql, [data.Nome, data.Contacto, data.UI, UserID], function(error, rows){
-		if(error) throw error;
-		callback(rows[0]);
-	});
-},
-
-remove(email, callback) {
+	remove(email, callback) {
 		var sql = "DELETE from `mydb`.`User` WHERE Email=?";
-		global.connection.query(sql, [email], function(error, rows, fields){
+		global.connection.query(sql, [email], function (error, rows, fields) {
 			if (error) throw error;
 			callback(rows);
 		});
 	},
-	
-/*--------------------PREFERENCIAS----------------------*/
 
-readpreferencias(email, callback) {
+	/*--------------------PREFERENCIAS----------------------*/
+
+	readpreferencias(email, callback) {
 		var sql = "SELECT Localidade from Associacao where Email_User=?";
-		global.connection.query(sql, [email], function(error, rows, fields) {
+		global.connection.query(sql, [email], function (error, rows, fields) {
 			if (error) throw error;
 			callback(rows[0]);
 		});
 	},
-	
-createpreferencias(email, data, callback) {
-	var sql = "INSERT INTO Associacao (Localidade, Email_User, tipoAssociacao) VALUES (?,?,?)";
 
-	global.connection.query(
-		sql, [data.Localidade, email, data.tipoAssociacao], function(error, rows, fields) {
-		if (error) throw error;
-		callback(rows[0]);
-		console.log(sql);
-	});
-},
+	createpreferencias(email, data, callback) {
+		var sql = "INSERT INTO Associacao (Localidade, Email_User, tipoAssociacao) VALUES (?,?,?)";
 
-/*--------------------FIM PREFERENCIAS-------------------*/
-	
-areValidCredentials(email, password, callback) {
+		global.connection.query(
+			sql, [data.Localidade, email, data.tipoAssociacao],
+			function (error, rows, fields) {
+				if (error) throw error;
+				callback(rows[0]);
+				console.log(sql);
+			});
+	},
+
+	/*--------------------FIM PREFERENCIAS-------------------*/
+
+	areValidCredentials(email, password, callback) {
 		var sql = "SELECT Password FROM User WHERE Email=?";
-		global.connection.query(sql, [email], function(error, rows){
+		global.connection.query(sql, [email], function (error, rows) {
 			if (error) throw error;
-			if (rows.length == 0 ) callback(false);
-			bcrypt.compare(rows[0].Password, password, function(err, res) {
+			if (rows.length == 0) callback(false);
+			bcrypt.compare(rows[0].Password, password, function (err, res) {
 				callback(true); // res === true
-});
+			});
 		});
-		}
-	
+	}
+
 };
-	
+
 /* return bcrypt.compareSync(password, this.password)
 	//New
 areValidCredentials(email, password, callback) {
